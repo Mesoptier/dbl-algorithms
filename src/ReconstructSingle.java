40a9e02c926abdfc3128c402839e4592434e5cc1
jamiro24 <jamiroleander24@gmail.com>
2016-04-29 11:16:11 +0200

    centered segment lines through points, added a basic single reconstruction to show display of lines

--- src/ReconstructSingle.java
+++ src/ReconstructSingle.java
@@ -1,11 +1,17 @@
 import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ReconstructSingle extends Reconstruct {
 
   @Override
   public ProblemOutput start(Point[] points) {
-    Segment[] segments = new Segment[0];
+    Segment[] segments = new Segment[points.length - 1];
+    //draw some lines to show drawing ability (remove later)
+    for (int i = 0; i < points.length - 1; i++) {
+      segments[i] = new Segment(points[i], points[i+1]);
+    }
     return new ProblemOutput(points, segments);
   }
-
 }
