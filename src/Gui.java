b82e45d35714b9498d81f246722736dd912c06cc
stan-roelofs <s.roelofs@student.tue.nl>
2016-04-29 11:36:57 +0200

    Implemented loading/saving of test cases

--- src/Gui.java
+++ src/Gui.java
@@ -1,10 +1,10 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.ByteArrayOutputStream;
-
+import java.io.*;
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class Gui implements ActionListener {
 
@@ -13,6 +13,12 @@ public class Gui implements ActionListener {
   private JTextArea inputText;
   private JTextArea outputText;
   private GuiProblemPanel problemPanel;
+
+  /** The file chooser for open and save dialogs. */
+  private final JFileChooser caseChooser = new JFileChooser();
+
+  /** Default directory for loading and saving of test cases. */
+  public static final File DEFAULT_DIRECTORY = new File(new File("."), "test");
 
   public Gui() {
     initGui();
@@ -66,7 +72,7 @@ public class Gui implements ActionListener {
     c.gridy = 0;
     infoPanel.add(inputPanel, c);
 
-    // --- Input text
+    // -- Input text
     inputText = new JTextArea();
     inputText.setFont(FONT_MONOSPACED);
     inputPanel.add(new JScrollPane(inputText), BorderLayout.CENTER);
@@ -83,6 +89,34 @@ public class Gui implements ActionListener {
     c.gridy = 1;
     c.insets = new Insets(5, 0, 5, 0);
     infoPanel.add(startButton, c);
+
+    // -- Menu
+    JMenuBar JMenuBar1 = new JMenuBar();
+    JMenu JMenuFile = new JMenu();
+    JMenuItem JMenuItemLoad = new JMenuItem();
+    JMenuItem JMenuItemSave = new JMenuItem();
+
+    JMenuFile.setText("File");
+
+    JMenuItemLoad.setText("Load");
+    JMenuItemLoad.setActionCommand("load");
+    JMenuItemLoad.addActionListener(this);
+    JMenuFile.add(JMenuItemLoad);
+
+    JMenuItemSave.setText("Save");
+    JMenuItemSave.setActionCommand("save");
+    JMenuItemSave.addActionListener(this);
+    JMenuFile.add(JMenuItemSave);
+
+    JMenuBar1.add(JMenuFile);
+
+    window.setJMenuBar(JMenuBar1);
+
+    // -- File chooser
+    caseChooser.setCurrentDirectory(DEFAULT_DIRECTORY);
+    FileNameExtensionFilter filter = new FileNameExtensionFilter(
+        "Test case files", "in");
+    caseChooser.setFileFilter(filter);
 
     // -- Output panel
     JPanel outputPanel = new JPanel();
@@ -106,8 +140,71 @@ public class Gui implements ActionListener {
     window.setVisible(true);
   }
 
+
+  private void loadCase() {
+    int result = caseChooser.showOpenDialog(null);
+
+    if (result != JFileChooser.APPROVE_OPTION) {
+      return; // canceled or error
+    }
+
+    File caseFile = caseChooser.getSelectedFile();
+    try {
+      InputStream inputStream = new FileInputStream(caseFile);
+
+      try {
+        inputText.setText("");
+        BufferedReader fromFile = new BufferedReader(new InputStreamReader(inputStream));
+        String line;
+        while ((line = fromFile.readLine()) != null) {
+          inputText.append(line + "\n");
+        }
+      } catch (IOException e) {
+        // do something
+        return;
+      }
+
+      startRunner();
+
+    } catch (FileNotFoundException e) {
+      // do something
+      return;
+    }
+  }
+
+  private void saveCase() {
+    int result = caseChooser.showSaveDialog(null);
+
+    if (result != JFileChooser.APPROVE_OPTION) {
+      return; // canceled or error
+    }
+
+    File caseFile = caseChooser.getSelectedFile();
+    if (caseFile.exists ()) {
+      int response = JOptionPane.showConfirmDialog(null,
+          "Overwrite existing file?",
+          "Confirm Overwrite",
+          JOptionPane.OK_CANCEL_OPTION,
+          JOptionPane.QUESTION_MESSAGE);
+      if (response == JOptionPane.CANCEL_OPTION) {
+        return;
+      }
+    }
+    // can write file
+    PrintWriter out;
+    try {
+      out = new PrintWriter(caseFile);
+      out.print(inputText.getText());
+      out.close();
+    } catch (FileNotFoundException e) {
+      // do something
+      return;
+    }
+  }
+
   private void startRunner() {
     ProblemInput input = ProblemInput.fromString(inputText.getText());
+
     problemPanel.setProblemInput(input);
 
     Runner runner = new Runner(input);
@@ -126,6 +223,11 @@ public class Gui implements ActionListener {
       case "startRunner":
         startRunner();
         break;
+      case "load":
+        loadCase();
+        break;
+      case "save":
+        saveCase();
       default:
     }
   }
