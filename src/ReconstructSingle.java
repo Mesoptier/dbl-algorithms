71ff391fde2c09efd1e045edd6852826e5512c78
jamiro24 <jamiroleander24@gmail.com>
2016-04-29 11:55:35 +0200

    added simple closest point for single reconstruction

--- src/ReconstructSingle.java
+++ src/ReconstructSingle.java
@@ -1,17 +1,49 @@
-import java.io.PrintStream;
-import java.util.Arrays;
 import java.util.HashSet;
-import java.util.Set;
+
 
 public class ReconstructSingle extends Reconstruct {
 
   @Override
   public ProblemOutput start(Point[] points) {
-    Segment[] segments = new Segment[points.length - 1];
-    //draw some lines to show drawing ability (remove later)
-    for (int i = 0; i < points.length - 1; i++) {
-      segments[i] = new Segment(points[i], points[i+1]);
-    }
+    Segment[] segments = new Segment[0];
+    segments = connect(points);
     return new ProblemOutput(points, segments);
   }
+
+  public Segment[] connect(Point[] points) {
+    int index = 0;
+    HashSet<Segment> segments = new HashSet<>();
+    while(true) {
+      points[index].setHasOutgoing(true);
+      int newIndex = closestPoint(points[index], points);
+      if (newIndex == -1) {
+        break;
+      }
+      segments.add(new Segment(points[index], points[newIndex]));
+
+      index = newIndex;
+      points[index].setHasIncoming(true);
+    }
+    return segments.toArray(new Segment[segments.size()]);
+  }
+
+  public int closestPoint(Point point, Point[] points) {
+    double smallestDist = Double.MAX_VALUE;
+    int index = -1;
+
+    for (int i = 0; i < points.length; i++) {
+      if (point != points[i] && !points[i].hasIncoming()) {
+        double newDistance = point.distanceSquared(points[i]);
+        System.out.println(point.toString() + " , " + newDistance);
+        if (newDistance < smallestDist) {
+          smallestDist = newDistance;
+          index = i;
+        }
+      }
+    }
+
+    return index;
+  }
+
+
 }
