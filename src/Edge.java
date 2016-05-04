3464eeb03af2c941dd2facb73880a45b0a96ea63
Koen Klaren <koen.klaren@gmail.com>
2016-05-04 13:46:39 +0200

    Make Edge conform to coding style, add copy constructor, and add mark property

--- src/Edge.java
+++ src/Edge.java
@@ -1,29 +1,42 @@
-public class Edge
-{
-    /** the origin of the edge */
-    private Vertex head;
-    /** the destination of the edge */
-    private Vertex tail;
-    /** constructor to create an edge with the given origin and destination */
-    public Edge(Vertex head, Vertex tail)
-    {
-        this.head = head;
-        this.tail = tail;
-    }
-    /** Returns the origin of the edge. */
-    public Vertex getHead()
-    {
-        return head;
-    }
-    /** Returns the destination of the edge. */
-    public Vertex getTail()
-    {
-        return tail;
-    }
-    /** Removes the endpoints of the edge. */
-    public void nullify()
-    {
-        this.head = null;
-        this.tail = null;
-    }
-} 
+public class Edge {
+
+  private Vertex head;
+  private Vertex tail;
+  private int mark;
+
+  public Edge(Vertex head, Vertex tail) {
+    this(head, tail, 0);
+  }
+
+  public Edge(Vertex head, Vertex tail, int mark) {
+    this.head = head;
+    this.tail = tail;
+    this.mark = mark;
+  }
+
+  public Edge(Edge edge) {
+    this(edge.head, edge.tail, edge.mark);
+  }
+
+  public Vertex getHead() {
+      return head;
+  }
+
+  public Vertex getTail() {
+      return tail;
+  }
+
+  public int getMark() {
+    return mark;
+  }
+
+  public void setMark(int mark) {
+    this.mark = mark;
+  }
+
+  public void nullify() {
+    this.head = null;
+    this.tail = null;
+  }
+
+}
